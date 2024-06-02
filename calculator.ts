c15de9d63ef446a6cf4ab5dc344b89dec7020ef1
Jacobo <tadeo.jacobo6@gmail.com>
2024-06-01 21:44:48 -0400

    calculator ts

---
+++ calculator.ts
@@ -0,0 +1,24 @@
+const operations = ['multiply', 'divide', 'add'];
+
+const calculator = (a: number, b: number, operator: string) => {
+    if (!operations.includes(operator)) {
+        console.log("This operation is not supported")
+    }
+    switch (operator) {
+        case 'multiply':
+            return a * b;
+        case 'divide':
+            if (b === 0) {
+                return 'You cannot divide by 0';
+            } else {
+                return a / b;
+            }
+        case 'add':
+            return a + b;
+        default:
+            return 'Invalid operator';
+    }
+}
+
+const result = calculator(6, 2, 'multiply')
+console.log("Your result is", result) 
